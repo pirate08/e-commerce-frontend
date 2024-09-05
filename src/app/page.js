754cf9fe10232f4cf6e5c1fa476099cfb06708c2
Mayukh <mayukhdebgoswami82@gmail.com>
2024-09-05 23:11:50 +0530

    MiddlePara has been added...

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import React from 'react';
 import OfferBanner from '@/components/OfferBanner';
 import HeroBanner from '@/components/HeroBanner';
 import HomeBanner from '@/components/HomeBanner';
+import Wrapper from '@/components/Wrapper';
 
 export default function Home() {
   return (
@@ -14,6 +15,20 @@ export default function Home() {
       <div className='py-10'>
         <HomeBanner />
       </div>
+      <Wrapper>
+        {/* heading and paragaph start */}
+        <div className='text-center max-w-[800px] mx-auto my-[50px] md:my-[80px]'>
+          <div className='text-[28px] md:text-[34px] mb-5 font-semibold leading-tight'>
+            Cushioning for Your Miles
+          </div>
+          <div className='text-md md:text-xl'>
+            A lightweight Nike ZoomX midsole is combined with increased stack
+            heights to help provide cushioning during extended stretches of
+            running.
+          </div>
+        </div>
+        {/* heading and paragaph end */}
+      </Wrapper>
     </div>
   );
 }
